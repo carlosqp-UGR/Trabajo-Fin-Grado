b2dd2fda5b42d85f9c6e31dfaabf51b35c13f54e
carlosqp-UGR <127781876+carlosqp-UGR@users.noreply.github.com>
2024-06-19 07:30:24 +0200

    Update config.js

--- app/src/config.js
+++ app/src/config.js
@@ -4,14 +4,14 @@ config();
 export const database = {
   // connectionLimit: 10,
   host: process.env.DATABASE_HOST || 'localhost',
-  user: process.env.DATABASE_USER || 'app',
-  password: process.env.DATABASE_PASSWORD || 'D9q$Zr7vXk',
-  database: process.env.DATABASE_NAME || 'db_app_aqh',
-  // port: process.env.DATABASE_PORT || 3306,
+  user: process.env.DATABASE_USER || 'app-user',
+  password: process.env.DATABASE_PASSWORD || 'app-passwd',
+  database: process.env.DATABASE_NAME || 'database',
+  port: process.env.DATABASE_PORT || 3306,
 };
 
 export const port = process.env.PORT || 80;
 
-export const production = process.env.production || true;
+export const PRODUCTION = process.env.PRODUCTION || true;
 
-export const SECRET = process.env.SECRET || 'some secret key';
+export const SECRET = process.env.SECRET || 'mySecret';
